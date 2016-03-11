8d235fec865849f8cd229bb1450c6c20bce51056
Natasha Carlyon <natasha.carlyon@gmail.com>
2016-03-11 14:39:25 +0000

    nitrous-backup at Fri Mar 11 14:39:25 UTC 2016

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+ # prime_array = []
+
+#def largest_prime_factor(input)
+ #       i = input
+  #      while i % 5 == 0
+   #             factor = input / 5
+    #            prime_array.insert(factor, 5)
+     #           i = factor
+      #  end
+       # prime_array.max
+# end
