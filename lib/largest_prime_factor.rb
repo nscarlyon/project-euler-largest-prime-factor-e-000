b6d8502ccf1383eed0b2a6df31651ff1f6e35f53
Natasha Carlyon <natasha.carlyon@gmail.com>
2016-03-18 13:09:59 +0000

    nitrous-backup at Fri Mar 18 13:09:59 UTC 2016

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,12 +1,26 @@
 # Enter your procedural solution here!
- # prime_array = []
 
-#def largest_prime_factor(input)
- #       i = input
-  #      while i % 5 == 0
-   #             factor = input / 5
-    #            prime_array.insert(factor, 5)
-     #           i = factor
-      #  end
-       # prime_array.max
-# end
+def largest_prime_factor(input)
+        while input % 2 == 0
+                factor = input / 2
+                input = factor
+                break if input == 2
+        end
+        while input % 5 == 0
+                factor = input / 5
+                input = factor
+                break if input == 5
+        end
+
+        while input % 7 == 0
+                factor = input / 7
+                input = factor
+                break if input == 7
+        end
+             while input % 13 == 0
+                factor = input / 13
+                input = factor
+                break if input == 13
+        end
+        return factor
+ end
