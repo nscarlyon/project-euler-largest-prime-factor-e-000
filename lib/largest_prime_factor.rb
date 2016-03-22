a0f83fbb89fe30239d9e5e3b6122541ea9c2be5c
Natasha Carlyon <natasha.carlyon@gmail.com>
2016-03-22 04:53:13 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,26 +1,12 @@
 # Enter your procedural solution here!
 
 def largest_prime_factor(input)
-        while input % 2 == 0
-                factor = input / 2
-                input = factor
-                break if input == 2
-        end
-        while input % 5 == 0
-                factor = input / 5
-                input = factor
-                break if input == 5
-        end
+        factor = input
+        (2..Math.sqrt(input)).each do |i|
 
-        while input % 7 == 0
-                factor = input / 7
-                input = factor
-                break if input == 7
-        end
-             while input % 13 == 0
-                factor = input / 13
-                input = factor
-                break if input == 13
+                while factor % i == 0 && factor > i
+                        factor = factor / i
+                end
         end
         return factor
- end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+        attr_reader :number
+
+        def initialize(input)
+                @number = largest_prime_factor(input)
+        end
+
+def largest_prime_factor(input)
+        factor = input
+        (2..Math.sqrt(input)).each do |i|
+
+                while factor % i == 0 && factor > i
+                        factor = factor / i
+                end
+        end
+        return factor
+end
+
+end
